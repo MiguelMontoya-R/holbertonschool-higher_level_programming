815fdd7ed72b2f9a13af80068ecb990deef11580
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-04-28 23:14:12 +0000

    '\' for multiple lines =D

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,7 @@ number = random.randint(-10000, 10000)
 if number < 0:
     last_digit = (number * -1) % 10
     last_digit *= -1
-elif number > 0:
-    last_digit = number % 10
-else:
+elif number => 0:
     last_digit = number % 10
 
 if last_digit > 5:
