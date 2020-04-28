2aee558e6579666a3ed37349bcefbcac07bdcb57
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-04-28 23:18:35 +0000

    '\' for multiple lines =D

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,7 +12,7 @@ if last_digit > 5:
  is greater than 5".format(number, last_digit))
 elif last_digit == 0:
     print("Last digit of {} is {} and\
- is zero".format(number, last_digit))
+ is 0".format(number, last_digit))
 elif last_digit < 6:
     print("Last digit of {} is {} and\
  is less than 6 and not 0".format(number, last_digit))
