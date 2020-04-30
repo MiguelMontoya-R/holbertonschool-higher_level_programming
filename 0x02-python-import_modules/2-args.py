31f6ea3f0b24e9f17757e85e0c6f2ddfa0a98166
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-04-30 21:28:10 +0000

    cases added (len(sys.argv) == 1 or len(sys.argv) == 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{:d} arguments".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
