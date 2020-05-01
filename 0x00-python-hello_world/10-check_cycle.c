1f75f7c7581a8e08d04c4170669f2035f732c0d1
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-01 20:43:46 +0000

    If tmp is not null

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * check_cycle - checks if there's a cycle in linked list
+ * @list: the list to check
+ * Return: 1 if cycle = true/ 0 if cycle = false
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *tmp = list;
 	listint_t *aux = list;
 
 	aux = aux->next;
-	while (aux)
+	while (aux && tmp)
 	{
 		if (tmp == aux)
 			return (1);
