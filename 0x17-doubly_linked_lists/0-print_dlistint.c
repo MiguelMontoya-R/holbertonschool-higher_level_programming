7c7e09d97f50a5cb5e94b1e467067f2d0fcab122
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-02 18:39:50 +0000

    backup

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_dlistint - print all elements of list
+ * @h: pointer to head of linked list
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	
+}
