37de821386920e7c74766daeb2d62a85cd56e1d7
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-02 18:53:18 +0000

    first task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,5 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	
+	const dlistint_t *ptr = h;
+	size_t counter = 0;
+
+	while (ptr)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		counter++;
+	}
+	return (counter);
 }
