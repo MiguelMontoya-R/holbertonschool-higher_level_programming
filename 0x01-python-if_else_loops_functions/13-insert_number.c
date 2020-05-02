cdaabef4dbf3bfcb2e4ecb85cf8e6efaa31209dc
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-02 05:24:12 +0000

    fixing betty with emacs

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * insert_node - insert node sorted
+ * @head: reference to linked list
+ * @number: number to add in linked list
+ * Return: The new node
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *tmp = *head;
-    listint_t *new_node;
+	listint_t *tmp = *head;
+	listint_t *new_node;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = number;
-    if (*head == NULL || tmp->n >= new_node->n)
-    {
-        *head = new_node;
-        new_node->next = *head;
-    }
-    else
-    {   
-        while (tmp->next)
-        {
-            if (tmp->next->n >= new_node->n)
-                break;
-            tmp = tmp->next;
-        }
-        new_node->next = tmp->next;
-        tmp->next = new_node;
-    }
-    return (new_node);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	if (*head == NULL || tmp->n >= new_node->n)
+	{
+		*head = new_node;
+		new_node->next = *head;
+	}
+	else
+	{
+		while (tmp->next)
+		{
+			if (tmp->next->n >= new_node->n)
+				break;
+			tmp = tmp->next;
+		}
+		new_node->next = tmp->next;
+		tmp->next = new_node;
+	}
+	return (new_node);
 }
