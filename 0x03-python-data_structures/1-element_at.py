b90add0b6d508db93eab056976b9e36ef2ab5572
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-05 21:35:32 +0000

    Adding cases in one line

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return
-    elif idx > len(my_list):
-        return
+    if idx < 0 or len(my_ist) < idx:
+        return None
     else:
         return (my_list[idx])
