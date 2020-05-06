031bf5ab4c7837d985f2dd0a3b49f414fad7388f
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-06 22:07:49 +0000

    Back to the old school

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        lenght = len(sentence)
-        first_letter = sentence[0:1]
-        return lenght, first_letter
+    if sentence:
+        return len(sentence), sentence[0]
+    else:
+        return len(sentence), None
