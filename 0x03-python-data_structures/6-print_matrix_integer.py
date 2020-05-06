a0e657e73cc1b6c365b1059015da735215693db9
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-06 07:28:55 +0000

    pep8 indentation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        print('\n'.join([' '.join(['{:d}'.format(item) for item in row])for row in matrix]))
+        print('\n'.join([' '.join(['{:d}'.format(item) for item in row])
+                        for row in matrix]))
         # Print matrix with .join
