03ef3864b9b9fa4c7d375b600914ea506fc4d6ce
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-06 01:38:08 +0000

    if my_list erased

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 def new_in_list(my_list, idx, element):
-    if my_list:
-        new_list = my_list.copy()
-        if idx >= 0 and idx < len(my_list):
-            new_list[idx] = element
-            return new_list
-        else:
-            return new_list
+    new_list = my_list.copy()
+    if idx >= 0 and idx < len(my_list):
+        new_list[idx] = element
+        return new_list
     else:
-        return
+        return new_list
