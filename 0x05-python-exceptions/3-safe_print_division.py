ae2e7d91ff4d2831c4a34a4b3d57b2c76d674650
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-18 22:30:17 +0000

    Another way of doing

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return result
+    except:
+        return
+    finally:
+        if b == 0:
+            result = None
+        print("Finally Result: {}".format(result))
