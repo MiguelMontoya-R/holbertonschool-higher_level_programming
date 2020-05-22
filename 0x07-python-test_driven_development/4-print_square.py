3b9952acf90bd4cb80e3297017dc508f79ada78a
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-22 22:24:48 +0000

    Pep8

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Function to print a square of #
+"""
+
+
+def print_square(size):
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    elif size == float and size < 0:
+        raise TypeError('size must be an integer')
+    else:
+        for i in range(size):
+            for j in range(size):
+                print('#', end='')
+            print()
