823b925ff7ff4b5e5a6ea7483515ee20905186b0
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-22 16:34:52 +0000

    0 task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+Addition of two numbers
+"""
 def add_integer(a, b=98):
-    if (a, b) != [int, float]:
-        print("Must be a number")
-    else:
-        return (a + b)
+    """ Addition of two numbers
+
+    Arguments:
+        a {int} -- Number
+
+    Keyword Arguments:
+        b {int} -- Number (default: {98})
+
+    Returns:
+        [int] -- The addition of 'a' and 'b'
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    elif type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    return (a + b)
