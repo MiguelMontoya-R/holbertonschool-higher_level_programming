9540281d3e65174ea3b574f8ce8d0d88043f359a
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-06-02 20:34:12 +0000

    Updating

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define class Ractangle
+    """
+
+
+class Rectangle:
+    """ Define empty class
+    """
+
+    def __init__(self):
+        self.__width = width
+        self.__height = height
+
