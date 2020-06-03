d5c644df460afe907748d0f3b47edced14fb135e
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-06-03 03:24:22 +0000

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+class MyList(list):
+    """[summary]
+
+    Arguments:
+        list {[type]} -- [description]
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
