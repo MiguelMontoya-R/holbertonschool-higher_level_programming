9ba0c9b581392fb45c541022c3e01b464323781b
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-06-09 16:38:28 +0000

    Fixing pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,11 +9,13 @@ class Square(Rectangle):
     Args:
         Rectangle ([type]): [description]
     """
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return '[Square] {} {}/{} - {}'.format(self.id, self.x, self.y, self.width)
+        return '[Square] {} {}/{} - {}'.format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
