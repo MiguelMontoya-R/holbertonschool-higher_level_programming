b2e1c039aac6b22e9f1a910c8299ade456a58e37
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-06-09 00:30:42 +0000

    First update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,10 @@ class Rectangle(Base):
                                                            self.__y,
                                                            self.__width,
                                                            self.__height)
+
+    def update(self, *args):
+        self.id = args[0]
+        self.__width = args[1]
+        self.__height = args[2]
+        self.__x = args[3]
+        self.__y = args[4]
