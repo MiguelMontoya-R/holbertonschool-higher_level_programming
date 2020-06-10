c7924c44cd6131da7606ad24d7d58a7f6c41abe1
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-06-10 04:07:36 +0000

    repeat previous task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,10 @@ class Square(Rectangle):
         else:
             for attr, value in kwargs.items():
                 setattr(self, attr, value)
+
+    def to_dictionary(self):
+        dic = {'id': self.id,
+               'size': self.size,
+               'x': self.x,
+               'y': self.y}
+        return dic
